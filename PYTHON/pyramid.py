f3b498f1fa299a018b03bc38d880d3d47e6dc94b
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2024-12-10 15:35:44 +0530

    pyramid

--- PYTHON/pyramid.py
+++ PYTHON/pyramid.py
@@ -28,6 +28,16 @@
 
 
 # square = lambda x : x ** 2
-
 # print(square(5))
+
+# pyramid
+# for i in range(5):
+#     for j in range(5-i):
+#         print(' ',end='')
+    
+#     for k in range(2 * i-1):
+#         print("*",end='')
+#     print()
+
+
 
