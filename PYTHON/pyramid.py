0475a40d200b6227d01a097c96c6f6a3b5df117e
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2024-12-10 12:34:14 +0530

    python basics covered

--- PYTHON/pyramid.py
+++ PYTHON/pyramid.py
@@ -1,4 +1,33 @@
 
-for i in range(5):
-    print("*" * i)
-    print()
+# for i in range(5):
+#     print("*" * i)
+#     print()
+
+
+
+# def dec(fun):
+#     def wrapper():
+#         print('first')
+#         fun()
+#         print('second')
+#     return wrapper
+
+# @dec
+# def sample():
+#     print("hello")
+# sample()
+
+# import copy
+# numbers= [1,2,3,[10,20]]
+
+# second_list = copy.deepcopy(numbers)
+# second_list[3][0]= 100
+
+# print(numbers)
+# print(second_list,'second')
+
+
+# square = lambda x : x ** 2
+
+# print(square(5))
+
