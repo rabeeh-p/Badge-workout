805efcb9212feeb51822dbbd7cca1d7d93719193
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2024-12-12 12:10:15 +0530

    oops added

--- PYTHON/workout.py
+++ PYTHON/workout.py
@@ -7,3 +7,39 @@
 #     print('success')
 # finally:
 #     print('finished')
+
+
+
+# sample =(1,2,3,2)
+
+# print(sample)
+# print(type(sample))
+# print(type(sample))
+# print(set(sample))
+
+
+# --------------------------------------- OOPS----------------------
+# POLYMORPHISAM
+# class Car:
+#     def tier(self):
+#         pass
+
+# class Thar(Car):
+
+#     def tier(self):
+#         print('4 tier')
+    
+
+# thar_obj =Thar()
+# thar_obj.tier()
+
+
+
+# class Myclass:
+#     name= 'python'
+#     @classmethod
+#     def sample(cls):
+#         print(f"ehllo{cls.name}")
+
+
+# Myclass.sample()
