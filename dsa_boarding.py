2293dbe893a9c083d9604fff5f506e4f6fb134c1
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-04-07 11:44:32 +0530

    boarding dsa started

---
+++ dsa_boarding.py
@@ -0,0 +1,16 @@
+matrix3d = [
+    [[1, 2], [3, 4]],
+    [[5, 6], [7]],
+    [[8]]
+]
+
+def flatten(arr):
+    result = []
+    for i in arr:
+        if isinstance(i, list):
+            result.extend(flatten(i))
+        else:
+            result.append(i)
+    return result
+
+print(flatten(matrix3d))
