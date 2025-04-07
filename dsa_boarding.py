72ed24c0343270c231d603be70d67c011410a5cc
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-04-07 23:37:48 +0530

    new file added

--- dsa_boarding.py
+++ dsa_boarding.py
@@ -1,16 +1,16 @@
-matrix3d = [
-    [[1, 2], [3, 4]],
-    [[5, 6], [7]],
-    [[8]]
-]
+# matrix3d = [
+#     [[1, 2], [3, 4]],
+#     [[5, 6], [7]],
+#     [[8]]
+# ]
 
-def flatten(arr):
-    result = []
-    for i in arr:
-        if isinstance(i, list):
-            result.extend(flatten(i))
-        else:
-            result.append(i)
-    return result
+# def flatten(arr):
+#     result = []
+#     for i in arr:
+#         if isinstance(i, list):
+#             result.extend(flatten(i))
+#         else:
+#             result.append(i)
+#     return result
 
-print(flatten(matrix3d))
+# print(flatten(matrix3d))
---
+++ python_boarding.py
@@ -0,0 +1,11 @@
+
+# import datetime
+from datetime import datetime,timedelta
+
+today = datetime.today()
+print(today)
+
+n = 10
+
+future = today + timedelta(days=n)
+print(future)
