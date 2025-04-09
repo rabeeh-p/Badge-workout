cb173982d1e96d6c04ffbe7f75e0521ad8b06880
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-04-09 23:25:30 +0530

    domain work complted

--- python_boarding.py
+++ python_boarding.py
@@ -1,11 +1,23 @@
 
 # import datetime
-from datetime import datetime,timedelta
+# from datetime import datetime,timedelta
 
-today = datetime.today()
-print(today)
+# today = datetime.today()
+# print(today)
 
-n = 10
+# n = 10
 
-future = today + timedelta(days=n)
-print(future)
+# future = today + timedelta(days=n)
+# print(future)
+
+
+
+def sample():
+    yield 10
+    yield 20
+
+
+new= sample()
+
+print(next(new))
+print(next(new))
