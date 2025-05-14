be3aa8ccdca371bf6b76d715461769ee44f0773c
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-05-14 22:40:04 +0530

    new code added

--- python_boarding.py
+++ python_boarding.py
@@ -1,9 +1,15 @@
 # new file created
 
 names= ['rabeeh','aliya','aban']
-for i,k in enumerate(names):
-    print(f'{i} index. value  {k}')
+# for i,k in enumerate(names):
+#     print(f'{i} index. value  {k}')
 
 
+
+
+
+for i in range(len(names)):
+    print(names[i])
+    
 
 
