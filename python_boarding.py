48aae21838213b477c32aa81e431476dcff6b7e7
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-06-08 19:35:09 +0530

    code cleaned

--- python_boarding.py
+++ python_boarding.py
@@ -10,18 +10,15 @@
 
 
 
-# Node class
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
 
-# LinkedList class
 class LinkedList:
     def __init__(self):
         self.head = None
 
-    # Insert at the end
     def append(self, data):
         new_node = Node(data)
         if not self.head:
@@ -33,7 +30,6 @@ class LinkedList:
             last = last.next
         last.next = new_node
 
-    # Print the list
     def display(self):
         current = self.head
         while current:
@@ -41,7 +37,6 @@ class LinkedList:
             current = current.next
         print("None")
 
-# Example usage
 ll = LinkedList()
 ll.append(10)
 ll.append(20)
