dfd1a97941ea6d5c17ff096d4e5438ab856ef93b
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-06-12 21:46:29 +0530

    updated

--- python_boarding.py
+++ python_boarding.py
@@ -97,22 +97,22 @@
 #     print(i,k)
 
 
-matrix3d = [
-    [[1, 2], [3, 4]],
-    [[5, 6], [7]],
-    [[8]]
-]
+# matrix3d = [
+#     [[1, 2], [3, 4]],
+#     [[5, 6], [7]],
+#     [[8]]
+# ]
 
-def flatten(arr):
-    result = []
-    for i in arr:
-        if isinstance(i, list):
-            result.extend(flatten(i))
-        else:
-            result.append(i)
-    return result
+# def flatten(arr):
+#     result = []
+#     for i in arr:
+#         if isinstance(i, list):
+#             result.extend(flatten(i))
+#         else:
+#             result.append(i)
+#     return result
 
-print(flatten(matrix3d))
+# print(flatten(matrix3d))
 
 
 
