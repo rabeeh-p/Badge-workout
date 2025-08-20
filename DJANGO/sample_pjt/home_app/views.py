59d1f1982c0c1a51efe23f1000c60650b2fb57e6
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-08-20 22:38:31 +0530

    django view command added

--- DJANGO/sample_pjt/home_app/views.py
+++ DJANGO/sample_pjt/home_app/views.py
@@ -19,6 +19,7 @@ class RegisterView(APIView):
             return Response({"message": "User created successfully!"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+# LOGIN
 class login(APIView):
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
