0d7caec9f5a7dd9cd92d71d478b21ecfd598d36f
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-08-23 19:38:40 +0530

    dsa linked list checked

--- python_boarding.py
+++ python_boarding.py
@@ -15,34 +15,34 @@
 #         self.data = data
 #         self.next = None
 
-# class LinkedList:
-#     def __init__(self):
-#         self.head = None
+class LinkedList:
+    def __init__(self):
+        self.head = None
 
-#     def append(self, data):
-#         new_node = Node(data)
-#         if not self.head:
-#             self.head = new_node
-#             return
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
 
-#         last = self.head
-#         while last.next:
-#             last = last.next
-#         last.next = new_node
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
 
-#     def display(self):
-#         current = self.head
-#         while current:
-#             print(current.data, end=' -> ')
-#             current = current.next
-#         print("None")
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=' -> ')
+            current = current.next
+        print("None")
 
-# ll = LinkedList()
-# ll.append(10)
-# ll.append(20)
-# ll.append(30)
+ll = LinkedList()
+ll.append(10)
+ll.append(20)
+ll.append(30)
 
-# ll.display()
+ll.display()
 
 
 
