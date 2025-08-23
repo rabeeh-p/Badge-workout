fba445d2570dc579b98c977f1337d6c1d84ef31b
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-08-23 19:37:06 +0530

    django registration function remvoed

--- DJANGO/sample_pjt/home_app/views.py
+++ DJANGO/sample_pjt/home_app/views.py
@@ -11,22 +11,15 @@ class HelloWorldView(APIView):
         return Response({"message": "Hello, world!"})
     
 
-class RegisterView(APIView):
+
+# LOGIN
+class login(APIView):
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
             return Response({"message": "User created successfully!"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-# LOGIN
-# class login(APIView):
-#     def post(self, request):
-#         serializer = RegisterSerializer(data=request.data)
-#         if serializer.is_valid():
-#             user = serializer.save()
-#             return Response({"message": "User created successfully!"}, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
 
