e030224ac487108697d23ee4f952390584192f70
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-08-23 19:37:44 +0530

    django url updated

--- DJANGO/sample_pjt/home_app/views.py
+++ DJANGO/sample_pjt/home_app/views.py
@@ -11,7 +11,14 @@ class HelloWorldView(APIView):
         return Response({"message": "Hello, world!"})
     
 
-
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({"message": "User created successfully!"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 # LOGIN
 class login(APIView):
     def post(self, request):
