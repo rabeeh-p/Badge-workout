dfe018024efb5e5d06afd0de451a4c58717758ed
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-08-23 19:39:24 +0530

    dsa flatten checked

--- python_boarding.py
+++ python_boarding.py
@@ -15,34 +15,34 @@
 #         self.data = data
 #         self.next = None
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
+# class LinkedList:
+#     def __init__(self):
+#         self.head = None
 
-    def append(self, data):
-        new_node = Node(data)
-        if not self.head:
-            self.head = new_node
-            return
+#     def append(self, data):
+#         new_node = Node(data)
+#         if not self.head:
+#             self.head = new_node
+#             return
 
-        last = self.head
-        while last.next:
-            last = last.next
-        last.next = new_node
+#         last = self.head
+#         while last.next:
+#             last = last.next
+#         last.next = new_node
 
-    def display(self):
-        current = self.head
-        while current:
-            print(current.data, end=' -> ')
-            current = current.next
-        print("None")
+#     def display(self):
+#         current = self.head
+#         while current:
+#             print(current.data, end=' -> ')
+#             current = current.next
+#         print("None")
 
-ll = LinkedList()
-ll.append(10)
-ll.append(20)
-ll.append(30)
+# ll = LinkedList()
+# ll.append(10)
+# ll.append(20)
+# ll.append(30)
 
-ll.display()
+# ll.display()
 
 
 
@@ -97,22 +97,22 @@ ll.display()
 #     print(i,k)
 
 
-# matrix3d = [
-#     [[1, 2], [3, 4]],
-#     [[5, 6], [7]],
-#     [[8]]
-# ]
+matrix3d = [
+    [[1, 2], [3, 4]],
+    [[5, 6], [7]],
+    [[8]]
+]
 
-# def flatten(arr):
-#     result = []
-#     for i in arr:
-#         if isinstance(i, list):
-#             result.extend(flatten(i))
-#         else:
-#             result.append(i)
-#     return result
+def flatten(arr):
+    result = []
+    for i in arr:
+        if isinstance(i, list):
+            result.extend(flatten(i))
+        else:
+            result.append(i)
+    return result
 
-# print(flatten(matrix3d))
+print(flatten(matrix3d))
 
 
 
