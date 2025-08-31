7952dc8b88bfa8a55488803e92f671c43af7863a
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-08-31 19:00:54 +0530

    register fn updated

--- DJANGO/sample_pjt/home_app/views.py
+++ DJANGO/sample_pjt/home_app/views.py
@@ -11,13 +11,13 @@ class HelloWorldView(APIView):
         return Response({"message": "Hello, rabeeh!"})
     
 
-class RegisterView(APIView):
-    def post(self, request):
-        serializer = RegisterSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            return Response({"message": "User created successfully!"}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# class RegisterView(APIView):
+#     def post(self, request):
+#         serializer = RegisterSerializer(data=request.data)
+#         if serializer.is_valid():
+#             user = serializer.save()
+#             return Response({"message": "User created successfully!"}, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 # LOGIN
 class login(APIView):
