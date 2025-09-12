8b7a391b35bdd3edb3c024e7f3728f75ba40dcc0
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-09-12 20:46:38 +0530

    pattern checked

--- python_boarding.py
+++ python_boarding.py
@@ -1,12 +1,12 @@
 # new file created
 
-# for i in range(5):
-#     print(i*i)
+for i in range(5):
+    print(i*i)
 
 
-# rows = 5
-# for i in range(1, rows + 1):
-#     print("*" * i)
+rows = 5
+for i in range(1, rows + 1):
+    print("*" * i)
 
 
 
