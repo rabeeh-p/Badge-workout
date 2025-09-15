98a0586732fda11b314f9ac97f61e502514cf119
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-09-15 22:14:17 +0530

    dsa hash table updated

--- sample_branch.py
+++ sample_branch.py
@@ -54,77 +54,77 @@
 # # obj.insert('pinappl',30)
 # obj.display()
                 
-# class HashTable:
-#     def __init__(self, size=2):
-#         self.size = size
-#         self.table = [None] * self.size  # Initialize with None for linear probing
-
-#     def hash_function(self, key):
-#         return hash(key) % self.size
-
-#     def insert(self, key, value):
-#         index = self.hash_function(key)
-
-#         # Linear probing: Find the next available slot
-#         while self.table[index] is not None:
-#             stored_key, _ = self.table[index]
-#             if stored_key == key:  # If key already exists, update value
-#                 self.table[index] = (key, value)
-#                 return
-#             index = (index + 1) % self.size  # Move to next slot
-
-#         # Insert new key-value pair
-#         self.table[index] = (key, value)
-
-#     def display(self):
-#         for i, bucket in enumerate(self.table):
-#             print(f'{i} == {bucket}')
-
-# # Example Usage
-# obj = HashTable()
-
-# obj.insert('apple', 10)
-# obj.insert('orange', 20)
-
-# obj.display()
-
-
-
 class HashTable:
-    def __init__(self,size=5):
-
+    def __init__(self, size=2):
         self.size = size
-        # self.table = [None] * size
+        self.table = [None] * self.size  # Initialize with None for linear probing
 
-        self.table= [[] for _ in range(size)]
-
-    
-    def hash_function(self,key):
+    def hash_function(self, key):
         return hash(key) % self.size
-    
-    def insert(self,key,value):
 
+    def insert(self, key, value):
         index = self.hash_function(key)
 
-        for i in self.table[index]:
-            if i[0] == key:
-                i[0] = value
+        # Linear probing: Find the next available slot
+        while self.table[index] is not None:
+            stored_key, _ = self.table[index]
+            if stored_key == key:  # If key already exists, update value
+                self.table[index] = (key, value)
                 return
-        
-        self.table[index].append([key,value])
+            index = (index + 1) % self.size  # Move to next slot
 
+        # Insert new key-value pair
+        self.table[index] = (key, value)
 
     def display(self):
-        for i,k in enumerate(self.table):
+        for i, bucket in enumerate(self.table):
+            print(f'{i} == {bucket}')
 
-            print(f'{i} => {k}')
-        
-
+# Example Usage
 obj = HashTable()
-obj.insert('apple',10)
-obj.insert('orange',10)
+
+obj.insert('apple', 10)
+obj.insert('orange', 20)
 
 obj.display()
+
+
+
+# class HashTable:
+#     def __init__(self,size=5):
+
+#         self.size = size
+#         # self.table = [None] * size
+
+#         self.table= [[] for _ in range(size)]
+
+    
+#     def hash_function(self,key):
+#         return hash(key) % self.size
+    
+#     def insert(self,key,value):
+
+#         index = self.hash_function(key)
+
+#         for i in self.table[index]:
+#             if i[0] == key:
+#                 i[0] = value
+#                 return
+        
+#         self.table[index].append([key,value])
+
+
+#     def display(self):
+#         for i,k in enumerate(self.table):
+
+#             print(f'{i} => {k}')
+        
+
+# obj = HashTable()
+# obj.insert('apple',10)
+# obj.insert('orange',10)
+
+# obj.display()
 
 
 # add = lambda x,b: x+ b
