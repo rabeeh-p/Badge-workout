79c5550cb2533a2b85486b9550b527ef9b1e72aa
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-09-16 22:16:31 +0530

    django register checked

--- DJANGO/sample_pjt/home_app/views.py
+++ DJANGO/sample_pjt/home_app/views.py
@@ -11,22 +11,22 @@ class HelloWorldView(APIView):
         return Response({"message": "Hello, rabeeh!"})
     
 
-class RegisterView(APIView):
-    def post(self, request):
-        serializer = RegisterSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            return Response({"message": "User created successfully!"}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-# LOGIN
-# class login(APIView):
+# class RegisterView(APIView):
 #     def post(self, request):
 #         serializer = RegisterSerializer(data=request.data)
 #         if serializer.is_valid():
 #             user = serializer.save()
 #             return Response({"message": "User created successfully!"}, status=status.HTTP_201_CREATED)
 #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+# LOGIN
+class login(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({"message": "User created successfully!"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
 
