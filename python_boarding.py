fb920b6ccf9a13efee87f561381ea98a1a6dd1f3
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-10-03 19:48:32 +0530

    enumerate checked

--- python_boarding.py
+++ python_boarding.py
@@ -47,54 +47,54 @@
 
 
 
-class HashTable:
-    def __init__(self, size):
-        self.size = size
-        self.table = [None] * size
+# class HashTable:
+#     def __init__(self, size):
+#         self.size = size
+#         self.table = [None] * size
 
-    def _hash(self, key):
-        return hash(key) % self.size
+#     def _hash(self, key):
+#         return hash(key) % self.size
 
-    def insert(self, key, value):
-        index = self._hash(key)
-        if self.table[index] is None:
-            self.table[index] = [(key, value)]
-        else:
-            for i, (k, v) in enumerate(self.table[index]):
-                if k == key:
-                    self.table[index][i] = (key, value)
-                    return
-            self.table[index].append((key, value))
+#     def insert(self, key, value):
+#         index = self._hash(key)
+#         if self.table[index] is None:
+#             self.table[index] = [(key, value)]
+#         else:
+#             for i, (k, v) in enumerate(self.table[index]):
+#                 if k == key:
+#                     self.table[index][i] = (key, value)
+#                     return
+#             self.table[index].append((key, value))
 
-    def get(self, key):
-        index = self._hash(key)
-        if self.table[index] is not None:
-            for k, v in self.table[index]:
-                if k == key:
-                    return v
-        return None
+#     def get(self, key):
+#         index = self._hash(key)
+#         if self.table[index] is not None:
+#             for k, v in self.table[index]:
+#                 if k == key:
+#                     return v
+#         return None
 
-    def delete(self, key):
-        index = self._hash(key)
-        if self.table[index] is not None:
-            for i, (k, _) in enumerate(self.table[index]):
-                if k == key:
-                    del self.table[index][i]
-                    return True
-        return False
+#     def delete(self, key):
+#         index = self._hash(key)
+#         if self.table[index] is not None:
+#             for i, (k, _) in enumerate(self.table[index]):
+#                 if k == key:
+#                     del self.table[index][i]
+#                     return True
+#         return False
 
-    def __str__(self):
-        return str(self.table)
-
-
+#     def __str__(self):
+#         return str(self.table)
 
 
 
 
-# names= ['rabeeh',23]
 
-# for i , k in enumerate(names):
-#     print(i,k)
+
+names= ['rabeeh',23]
+
+for i , k in enumerate(names):
+    print(i,k)
 
 
 # matrix3d = [
