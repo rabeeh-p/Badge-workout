d6cc05ffaedca7f3da36bb2d5081b556348f9f13
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-10-03 19:50:34 +0530

    updated

--- DJANGO/sample_pjt/home_app/views.py
+++ DJANGO/sample_pjt/home_app/views.py
@@ -6,11 +6,16 @@ from rest_framework import status
 # Create your views here.
 
 
-class HelloWorldView(APIView):
-    def get(self, request):
-        return Response({"message": "Hello, rabeeh!"})
-    
 
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({"message": "User created successfully!"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 
 # LOGIN
 class login(APIView):
