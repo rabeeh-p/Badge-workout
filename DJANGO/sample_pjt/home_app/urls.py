71cd2addd348dc1fb014843e80cf7301fa8ffecb
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-10-03 20:11:01 +0530

    django url updated

--- DJANGO/sample_pjt/home_app/urls.py
+++ DJANGO/sample_pjt/home_app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('hello/', HelloWorldView.as_view(), name='hello-world'),
+    path('hello/', HelloWorldView.as_view(), name='hello-WORLD'),
     path('register/', RegisterView.as_view(), name='register-url'),
 ]
