db10a559338a5c7630fc44701672710cddeaf242
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-10-09 20:48:47 +0530

    updated

--- DJANGO/sample_pjt/home_app/urls.py
+++ DJANGO/sample_pjt/home_app/urls.py
@@ -5,4 +5,8 @@ from .views import *
 urlpatterns = [
     path('hello/', HelloWorldView.as_view(), name='hello-world'),
     path('register/', RegisterView.as_view(), name='register-url'),
+
+
+    
+    path('register/', RegisterView.as_view(), name='register-url'),
 ]
