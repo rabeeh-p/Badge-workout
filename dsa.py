0c7ac13dc0163cf4e04f08563367883d26089c2f
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-11-09 23:45:05 +0530

    test : prime  list

--- dsa.py
+++ dsa.py
@@ -134,12 +134,12 @@
 
 
 
-# for i in range(2, 100):
-#     for k in range(2, i):
-#         if i % k == 0:
-#             break  # Not a prime number
-#     else:
-#         print(i)  # Prime number
+for i in range(2, 100):
+    for k in range(2, i):
+        if i % k == 0:
+            break  # Not a prime number
+    else:
+        print(i)  # Prime number
 
 
 
@@ -147,12 +147,12 @@
  
 
 
-from itertools import permutations
+# from itertools import permutations
 
-result = [ ''.join(p) for p in permutations('rab')]
+# result = [ ''.join(p) for p in permutations('rab')]
 
 
-# print(result)
+# # print(result)
 
 
 
