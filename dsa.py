3dcd3875d101842a854f194a430e088c5feabe6a
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2026-01-19 22:17:31 +0530

    test : lambda

--- dsa.py
+++ dsa.py
@@ -53,18 +53,17 @@
 # print(current)
 
 
-# swap = lambda s: s[-1] + s[1:-1] + s[0] if len(s) > 1 else s
-# print(swap("rabeeh"))
+swap = lambda s: s[-1] + s[1:-1] + s[0] if len(s) > 1 else s
+print(swap("rabeeh"))
 
 
-# import random
-# print(random.randint(0, 100))
+import random
+print(random.randint(0, 100))
 
 
-
-lst = [[1, 2], [3, 4,2,2,2], [5]]
-flat = [item for sublist in lst for item in sublist]
-print(flat)
+# lst = [[1, 2], [3, 4,2,2,2], [5]]
+# flat = [item for sublist in lst for item in sublist]
+# print(flat)
 
 class HashTable:
     def __init__(self,size=10):
