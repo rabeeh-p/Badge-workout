7dc476015e3fe1da4295ea58c0f5e99d4b9d141c
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2026-02-03 22:38:30 +0530

    test : prime update

--- dsa.py
+++ dsa.py
@@ -133,12 +133,14 @@ print(result(10,20))
 
 
 
-# for i in range(2, 100):
-#     for k in range(2, i):
-#         if i % k == 0:
-#             break  # Not a prime number
-#     else:
-#         print(i)  # Prime number
+for i in range(2, 100):
+    for k in range(2, i):
+        if i % k == 0:
+            break  # Not a prime number
+    else:
+        print(i)  # Prime number
+
+
 
 
 
